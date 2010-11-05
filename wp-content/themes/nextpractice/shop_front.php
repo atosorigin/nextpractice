bc12fded34c7cac7d19673371ec4edebb60f5231
simondelliott <simondelliott@gmail.com>
2010-11-05 15:09:47 +0000

    reviewed with Carl

--- wp-content/themes/nextpractice/shop_front.php
+++ wp-content/themes/nextpractice/shop_front.php
@@ -19,26 +19,29 @@ get_header(); ?>
           <div class="cell clickable" onclick="location.href='/human-factors/'"></div>
           <div class="cell text">
               <p>
-                  <?php bloginfo('description'); ?>
+                  We are a strategy and change consultancy, specialising in working with clients
+                  to address complex situations, where the answers are unknown, unclear or finely
+                  balanced. By strategy, we mean making sense of and solving complex problems
+                  and defining how to address major opportunities. By change, we mean realising the
+                  answers.
               </p>
-              <!--p>
-            decisions and actions that are successful come through addressing
-            real-world complexity. take multiple perspectives to tackle the
-            biggest problems. we share a common outlook with our cleints and
-            they, like us, have found that best practice used for management is
-            insufficient for their challenge and aspirations. We serve our
-            clients by addressing a specific issue or opportunity, leverageing
-            one or more of our famous five practices.
-              </p-->
           </div>
 
           <div class="cell"></div>
           <div class="cell clickable" onclick="location.href='/operations/'"></div>
           <div class="cell"></div>
-          <div class="cell clickable" onclick="location.href='/business-strategy/'"></div>
+          <div class="cell text">
+              <p>
+                We tend to work with executives and their teams who have found, like us, that the
+                best practices and knowledge typically used for management, strategy and change
+                to be insufficient for their challenges and aspirations. In a straightforward sense, our
+                clients report they experience poor outcomes with major initiatives despite all the best
+                practices deployed into the situation. Working together, we achieve better results.
+              </p>
+          </div>
 
           <div id="footer">
-                Next Practice Advisory is the Strategy & Change practice of Atos Consulting Ltd, formerly KPMG Consulting  &copy;2010.
+                Next Practice Consulting is the Strategy & Change unit of Atos Consulting Ltd, formerly KPMG Consulting  &copy;2010.
           </div>
 
       </div>
